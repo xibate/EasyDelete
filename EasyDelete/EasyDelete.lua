eebec4759150bc9abc17f961df4c4a086959837d
Jasper <jasper.huijgen@gmail.com>
2023-09-07 12:54:10 +0200

    Revert "Updated for 10.1.7 and added function for quest items"
    This reverts commit 63cac861de1e864bea24930ea3b14d380bbe6185.

--- EasyDelete/EasyDelete.lua
+++ EasyDelete/EasyDelete.lua
@@ -1,2 +1,1 @@
 hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_ITEM"],"OnShow",function(s) s.editBox:SetText(DELETE_ITEM_CONFIRM_STRING) end)
-hooksecurefunc(StaticPopupDialogs["DELETE_GOOD_QUEST_ITEM"],"OnShow",function(s) s.editBox:SetText(DELETE_ITEM_CONFIRM_STRING) end)
